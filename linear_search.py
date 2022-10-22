524f4a631b6b70514aa7ebd7acc27fe3dc65e086
Madan Devkota <88671618+DevkotaMadan@users.noreply.github.com>
2022-10-21 23:09:27 -0700

    linear search and binary search

---
+++ linear_search.py
@@ -0,0 +1,9 @@
+def linear_search(list, target):
+    """
+    returns the index postition of the target if found, else returns None
+    """
+    for i in range(o, len(list)):
+        if list[i]==target:
+            return i
+    return None
+        
